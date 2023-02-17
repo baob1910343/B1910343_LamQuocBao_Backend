d1f144b43138ceb64ce9595122b20680d14689f4
baob1910343 <baob1910343@student.ctu.edu.vn>
2023-02-17 15:53:50 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-const contactRouter = require("./app/routes/contact.route");
-const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 
+const ApiError = require("./app/api-error");
+const app = express();
+app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal server Error",
+    });
+});
 app.use(cors());
 app.use(express.json());
-app.use("/api/contacts", contactRouter);
+app.use("/api/contacts", contactsRouter);
 app.get("/", (req, res) => {
-    res.json({message : " Wellcome to contact book application." });
+    res.json({ message : " Wellcome to contact book application." });
 });
 module.exports = app;
